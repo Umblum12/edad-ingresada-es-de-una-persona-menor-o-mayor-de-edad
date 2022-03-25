ef2bbc08951610c812f148c831c92b5df900ad03
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:26:19 -0700

    Agregar archivos de proyecto.

---
+++ edad ingresada es de una persona menor o mayor de edad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace edad_ingresada_es_de_una_persona_menor_o_mayor_de_edad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 18;
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
+
+            if(edad >= 18)
+            {
+                Console.WriteLine("Adelante, pudes pasar porque eres mayor de edad");
+            }
+        }
+    }
+}
